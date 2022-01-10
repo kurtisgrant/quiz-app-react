5e555ddd3adbed0a18b775c5d1ff40b8f6ab11cd
Justin Huang <justin.2260@gmail.com>
2022-01-10 12:43:48 -0500

    changed users from sample users to ones in our database

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 /*
  * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
+ * Since this file is loaded in server.js into users,
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
@@ -21,5 +21,19 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/:id", (req, res) => {
+    db.query(`SELECT users.*, quizzes.* FROM users LEFT JOIN quizzes ON users.id = owner_id WHERE users.id = $1;`, [req.params.id])
+    .then(data => {
+      const user = data.rows;
+      res.json({ user });
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
+  });
+
   return router;
 };
