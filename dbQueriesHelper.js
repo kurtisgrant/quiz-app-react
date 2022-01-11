e1f3662a701c16c835e2fe843c6be30e09858fca
Justin Huang <justin.2260@gmail.com>
2022-01-11 00:20:48 -0500

    add function to retrieve user's quizzes and to find a specific quiz

--- dbQueriesHelper.js
+++ dbQueriesHelper.js
@@ -38,8 +38,42 @@ const getHomepage = function(db) {
     });
 }
 
+//get all quizzes owned by user
+const getUserQuizzes = function(db, id) {
+  return db
+    .query(`SELECT *
+            FROM quizzes
+            WHERE owner_id = $1`,
+            [id])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+//get data for a specific quiz
+const getQuiz = function(db, quizIdentifier) {
+  return db
+    .query(`SELECT quizzes.title, quizzes.description, questions.question, question_options.answer
+            FROM quizzes
+            JOIN questions ON quizzes.id = quiz_id
+            JOIN question_options ON questions.id = question_id
+            WHERE quiz_identifier = $1;`,
+            [quizIdentifier])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
 module.exports = {
   getUsers,
   getUserWithId,
-  getHomepage
+  getHomepage,
+  getUserQuizzes,
+  getQuiz
 }
