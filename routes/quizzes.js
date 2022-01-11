f7441c6a8f7952ca4fc8b5775c6974afce92d06f
Justin Huang <justin.2260@gmail.com>
2022-01-11 14:52:13 -0500

    add get route for /quizzes for specific user to see their own made quizzes

---
+++ routes/quizzes.js
@@ -0,0 +1,31 @@
+/*
+ * All routes for quizzes are defined here
+ * Since this file is loaded in server.js into quizzes,
+ *   these routes are mounted onto /quizzes
+ */
+
+const express = require('express');
+const { getUserQuizzes, getQuiz, addQuiz, generateQuizIdentifier } = require('../dbQueriesHelpers');
+const router  = express.Router();
+
+//export data from quizzes routes to be used by server.js
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    //testing function with hardcoding userId; remember to change using req.session!
+    const userId = 1;
+
+    getUserQuizzes(db, userId)
+      .then((quizzes) => {
+        res.json({ quizzes })
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ error: err.message} );
+      });
+  });
+
+
+
+  return router;
+};
