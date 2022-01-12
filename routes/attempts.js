716e5cd478303e86d9c00a32f1c815ff6999cf9f
Justin Huang <justin.2260@gmail.com>
2022-01-12 12:22:30 -0500

    add hard code for /attempts/:id to test page

--- routes/attempts.js
+++ routes/attempts.js
@@ -16,11 +16,11 @@ module.exports = (db) => {
     if (req.user) {
       const quizzes = [
         {id: 10, quiz_title: "Sandwiches Quiz", quiz_description: "A quiz about the best sandwiches", timestamp: "2:45pm Dec 20, 2021", score: "80%"},
-        {id: 20, quiz_title: "Pizza Quiz", quiz_description: "A quiz about the best pizzas", timestamp: "3:00 am Oct 31, 2021", score: "10%"},
+        {id: 20, quiz_title: "Pizza Quiz", quiz_description: "A quiz about the best pizzas", timestamp: "3:00 am Oct 31, 2021", score: "67%"},
         {id: 30, quiz_title: "Burger Quiz", quiz_description: "A quiz about the best burgers", timestamp: "2:15 pm Nov 10, 2021", score: "100%"}
       ]
 
-      const templateVars = { user:user, attempts: quizzes };
+      const templateVars = { user: user, attempts: quizzes };
       res.render("attempts", templateVars);
     } else {
       res.send("Please login to see your quiz attempts!");
@@ -51,24 +51,46 @@ module.exports = (db) => {
     const user = req.user;
 
     if (req.user) {
-      getQuizAttempt(db, req.params.id)
-        .then((attempt) => {
-          // if quiz taker is the same as the user logged in, render the specific quiz attempt, else prevent from seeing
-          if (attempt[0].tester_id === user.id) {
-            const templateVars = {user: req.user, attempts: attempt}
-            res.render("attempts", templateVars);
-          } else {
-            res.send("blank");
-          };
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-          });
+      const attempt = [{
+        quiz_id: 20,
+        quiz_title: "Pizza Quiz",
+        quiz_description: "A quiz about the best pizzas",
+        time: "3:00 am Oct 31, 2021",
+        questions: [
+          {question_id: 10, question_text: "Which pizza is this?", options: [{option_id: 30, option_text: "Mushroom mush pizza!"}, {option_id: 31, option_text: "Pizza piz pizza!"}, {option_id: 32, option_text: "Bacon bawk pizza!"}], correct_option_id: 31, selected_option_id: 31},
+          {question_id: 11, question_text: "Which pizza is this?", options: [{option_id: 33, option_text: "Tomato egg pizza!"}, {option_id: 34, option_text: "Hawaiian pizza!"}, {option_id: 35, option_text: "Nice pizza!"}], correct_option_id: 35, selected_option_id: 33},
+          {question_id: 12, question_text: "Which pizza is this?", options: [{option_id: 36, option_text: "Bad pizza!"}, {option_id: 37, option_text: "Smile pizza!"}, {option_id: 38, option_text: "Bye bye pizza!"}], correct_option_id: 36, selected_option_id: 36}
+        ],
+        score: "67%"
+      }];
+
+      const templateVars = { user: user, attempts: attempt };
+      res.render("attempts", templateVars);
     } else {
-      res.send("Please login to see your quiz attempts");
+      res.send("Please login to see your quiz attempts!");
     }
+
+    // const user = req.user;
+
+    // if (req.user) {
+    //   getQuizAttempt(db, req.params.id)
+    //     .then((attempt) => {
+    //       // if quiz taker is the same as the user logged in, render the specific quiz attempt, else prevent from seeing
+    //       if (attempt[0].tester_id === user.id) {
+    //         const templateVars = {user: req.user, attempts: attempt}
+    //         res.render("attempts", templateVars);
+    //       } else {
+    //         res.send("blank");
+    //       };
+    //     })
+    //     .catch(err => {
+    //       res
+    //         .status(500)
+    //         .json({ error: err.message });
+    //       });
+    // } else {
+    //   res.send("Please login to see your quiz attempts");
+    // }
   });
 
   router.post("/", (req, res) => {
