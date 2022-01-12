1021ee60d7b4d89e33cff9a2c1e06069f2bcb683
Justin Huang <justin.2260@gmail.com>
2022-01-12 12:59:34 -0500

    change variables to match ejs files for testing

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -16,13 +16,13 @@ module.exports = (db) => {
 
     if (req.user) {
       const quizzes = [
-        { quiz_identifier: 'biK50vH', title: 'Capital Cities of North America', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi magnam sit omnis repellat, sunt dolorum vitae quae modi, odio officiis libero cumque assumenda commodi ducimus.', avg_score: '67%', total_attempts: 10 },
-        { quiz_identifier: 'hv38vnj', title: 'Harry Potter Quiz', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Gesunt dolorum vitae quae modi, odio officiis libero cumque assumenda commodi ducimus.', avg_score: '83%', total_attempts: 3 },
+        { quiz_identifier: 'biK50vH', title: 'How are you?', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi magnam sit omnis repellat, sunt dolorum vitae quae modi, odio officiis libero cumque assumenda commodi ducimus.', avg_score: '67%', total_attempts: 10 },
+        { quiz_identifier: 'hv38vnj', title: 'You are how?', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Gesunt dolorum vitae quae modi, odio officiis libero cumque assumenda commodi ducimus.', avg_score: '83%', total_attempts: 3 },
         { quiz_identifier: 'fibajio', title: 'Parts of a Cell', description: 'Lorem ipsum dolor sit amet consectetur. Nisi magnam sit omnis repellat, sunt dolorum vitae quae modi, odio officiis libero cumque assumenda commodi ducimus.', avg_score: '72%', total_attempts: 1000 }
       ];
       const templateVars = { user, quizzes };
 
-      res.render("index", templateVars);
+      res.render("quizzes", templateVars);
     } else {
       res.send("Please login to see your quizzes!");
     }
@@ -50,7 +50,7 @@ module.exports = (db) => {
     const user = req.user;
 
     if (req.user) {
-      const quiz = [{
+      const questions = {
         title: "bleh",
         description: "BLEHBLEHBLHE aoufege fejfow",
         questions: [
@@ -58,9 +58,9 @@ module.exports = (db) => {
           {question_id: 67, text: "Byeeeee?", options: [{option_id: 93, option_text: "HIII"}, {option_id: 94, option_text: "HALLOOOO"}, {option_id: 95, option_text: "BYEEEE!"}]},
           {question_id: 68, text: "HB?", options: [{option_id: 96, option_text: "H"}, {option_id: 97, option_text: "HA"}, {option_id: 98, option_text: "B!"}]}
         ]
-      }];
-      const templateVars = { user, quiz };
-      res.render("", templateVars);
+      };
+      const templateVars = { user, questions };
+      res.render("quiz", templateVars);
 
     } else {
       res.send("Please login to complete this quiz!");
