68b5f4c23d1c9a31f7dc72c094b096c6e68fa655
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-01-13 00:32:23 +0000

    Move quiz title/description card out of script and into ejs template

--- public/scripts/create_quiz.js
+++ public/scripts/create_quiz.js
@@ -1,21 +1,5 @@
 
 $(() => {
-  const aboutQuizHTML = `
-  <div class="box">
-    <div class="field">
-      <label class="label">Title</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="Quiz Title">
-      </div>
-    </div>
-    <div class="field">
-      <label class="label">Description</label>
-      <div class="control">
-        <textarea class="textarea" placeholder="Quiz Description"></textarea>
-      </div>
-    </div>
-  </div>
-  `;
   const newQuestion = `
     <div class="card q-card my-6">
       <div class="card-header is-flex is-justify-content-space-betweeen is-align-items-center">
@@ -23,41 +7,37 @@ $(() => {
         <button class="delete delete-question mx-4 is-medium"></button>
       </div>
       <div class="card-content">
-
-      <div class="field">
-        <div class="control">
-          <textarea name="question" class="textarea" id="q-1" rows="2" placeholder="Question"></textarea>
+        <div class="field">
+          <div class="control">
+            <textarea name="question" class="textarea" id="q-1" rows="2" placeholder="Question"></textarea>
+          </div>
         </div>
+        <div class="q-options container mr-6 mt-5 has-background-blue">
+          <div class="q-option correct-option my-3">
+            <input class="input is-primary" type="text" placeholder="Correct Answer">
+            <button class="delete delete-option mx-3 is-invisible"></button>
+          </div>
+          <div class="q-option incorrect-option my-3">
+            <input class="input is-info" type="text" placeholder="Incorrect Answer">
+            <button class="delete delete-option mx-3 is-invisible"></button>
+          </div>
+        </div>
+        <button class="add-option button is-primary has-text-weight-bold">Add Option</button>
       </div>
-
-      <div class="q-options container mr-6 mt-5 has-background-blue">
-
-        <div class="q-option correct-option my-3">
-          <input class="input is-primary" type="text" placeholder="Correct Answer">
-          <button class="delete delete-option mx-3 is-invisible"></button>
-        </div>
-        <div class="q-option incorrect-option my-3">
-          <input class="input is-info" type="text" placeholder="Incorrect Answer">
-          <button class="delete delete-option mx-3 is-invisible"></button>
-        </div>
-
-      </div>
-
-      <button class="add-option button is-primary has-text-weight-bold">Add Option</button>
-
     </div>
-  </div>
-    `;
+  `;
   const newOption = `
-  <div class="q-option incorrect-option my-3">
-    <input class="input is-info" type="text" placeholder="Incorrect Answer">
-    <button class="delete delete-option mx-3"></button>
-  </div>
+    <div class="q-option incorrect-option my-3">
+      <input class="input is-info" type="text" placeholder="Incorrect Answer">
+      <button class="delete delete-option mx-3"></button>
+    </div>
   `;
 
+  // Append first question card
   const $questionsContainer = $('#questions-container');
-  $questionsContainer.append(aboutQuizHTML);
   $questionsContainer.append(newQuestion).find('.delete-question').addClass('is-invisible');
+
+  // Handle add/remove option and remove question click events
   $questionsContainer.on('click', (e) => {
     const $target = $(e.target);
     if ($target.hasClass('add-option')) {
@@ -69,11 +49,10 @@ $(() => {
       $target.closest('.q-card').remove();
     }
   });
+
+  // Handle add question click event
   $('#add-question').on('click', () => {
     $questionsContainer.append(newQuestion);
   });
-
-
-
 
 });
