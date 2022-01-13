2f9ebf67da4d9954730cdd0e03083812e88b1f34
Justin Huang <justin.2260@gmail.com>
2022-01-13 11:34:38 -0500

    change pages taking hard coded data to retrieving data from the database

--- routes/attempts.js
+++ routes/attempts.js
@@ -42,53 +42,53 @@ module.exports = (db) => {
 
     } else {
       res.send("Please login to see your quiz attempts");
-    }
+    };
   });
 
   router.get("/:id", (req, res) => {
-    const user = req.user;
-
-    if (req.user) {
-      const attempt = {
-        quiz_id: 20,
-        quiz_title: "Pizza Quiz",
-        quiz_description: "A quiz about the best pizzas",
-        time: "3:00 am Oct 31, 2021",
-        questions: [
-          { question_id: 10, question_text: "Which pizza is this?", options: [{ option_id: 30, option_text: "Mushroom mush pizza!" }, { option_id: 31, option_text: "Pizza piz pizza!" }, { option_id: 32, option_text: "Bacon bawk pizza!" }], correct_option_id: 31, selected_option_id: 31 },
-          { question_id: 11, question_text: "Which pizza is this?", options: [{ option_id: 33, option_text: "Tomato egg pizza!" }, { option_id: 34, option_text: "Hawaiian pizza!" }, { option_id: 35, option_text: "Nice pizza!" }], correct_option_id: 35, selected_option_id: 33 },
-          { question_id: 12, question_text: "Which pizza is this?", options: [{ option_id: 36, option_text: "Bad pizza!" }, { option_id: 37, option_text: "Smile pizza!" }, { option_id: 38, option_text: "Bye bye pizza!" }], correct_option_id: 36, selected_option_id: 36 }
-        ],
-        score: "67%"
-      };
-
-      const templateVars = { user, attempt };
-      res.render("attempt", templateVars);
-    } else {
-      res.send("Please login to see your quiz attempts!");
-    }
-
     // const user = req.user;
 
     // if (req.user) {
-    //   getQuizAttempt(db, req.params.id)
-    //     .then((attempt) => {
-    //       // if quiz taker is the same as the user logged in, render the specific quiz attempt, else prevent from seeing
-    //       if (attempt[0].tester_id === user.id) {
-    //         const templateVars = {user: req.user, attempts: attempt}
-    //         res.render("attempts", templateVars);
-    //       } else {
-    //         res.send("blank");
-    //       };
-    //     })
-    //     .catch(err => {
-    //       res
-    //         .status(500)
-    //         .json({ error: err.message });
-    //       });
+    //   const attempt = {
+    //     quiz_id: 20,
+    //     quiz_title: "Pizza Quiz",
+    //     quiz_description: "A quiz about the best pizzas",
+    //     time: "3:00 am Oct 31, 2021",
+    //     questions: [
+    //       { question_id: 10, question_text: "Which pizza is this?", options: [{ option_id: 30, option_text: "Mushroom mush pizza!" }, { option_id: 31, option_text: "Pizza piz pizza!" }, { option_id: 32, option_text: "Bacon bawk pizza!" }], correct_option_id: 31, selected_option_id: 31 },
+    //       { question_id: 11, question_text: "Which pizza is this?", options: [{ option_id: 33, option_text: "Tomato egg pizza!" }, { option_id: 34, option_text: "Hawaiian pizza!" }, { option_id: 35, option_text: "Nice pizza!" }], correct_option_id: 35, selected_option_id: 33 },
+    //       { question_id: 12, question_text: "Which pizza is this?", options: [{ option_id: 36, option_text: "Bad pizza!" }, { option_id: 37, option_text: "Smile pizza!" }, { option_id: 38, option_text: "Bye bye pizza!" }], correct_option_id: 36, selected_option_id: 36 }
+    //     ],
+    //     score: "67%"
+    //   };
+
+    //   const templateVars = { user, attempt };
+    //   res.render("attempt", templateVars);
     // } else {
-    //   res.send("Please login to see your quiz attempts");
+    //   res.send("Please login to see your quiz attempts!");
     // }
+
+    const user = req.user;
+
+    if (user) {
+      getQuizAttempt(db, req.params.id)
+        .then((attempt) => {
+          // if quiz taker is the same as the user logged in, render the specific quiz attempt, else prevent from seeing
+          if (attempt[0].tester_id === user.id) {
+            const templateVars = {user, attempt}
+            res.render("attempt", templateVars);
+          } else {
+            res.send("blank");
+          };
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+          });
+    } else {
+      res.send("Please login to see your quiz attempts");
+    }
   });
 
   router.post("/", (req, res) => {
